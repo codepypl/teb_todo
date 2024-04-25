f5bfa9d2dfd47c45d95ff949de6e002384aeac1c
Adam Dąbrowski <adam@codepy.pl>
2024-04-25 22:44:11 +0200

    dodano okno dodawania priorytatow

--- ui/options/priority.py
+++ ui/options/priority.py
@@ -16,9 +16,22 @@ class PriorityManagerApp(QDialog):
         layout.addWidget(self.priority_list)
 
         self.load_priorities()
+
+        self.add_button = QPushButton("Dodaj priorytet")
+        self.add_button.clicked.connect(self.add_priority)
+        layout.addWidget(self.add_button)
+
         self.setLayout(layout)
     def load_priorities(self):
         self.priority_list.clear()
         priorities = session.query(Priority).all()
         for priority in priorities:
             self.priority_list.addItem(str(priority.name))
+
+    def add_priority(self):
+        priority, ok = QInputDialog.getText(self, 'Dodaj priorytet', 'Wpisz nazwę priorytetu:')
+        if ok and priority:
+            new_priority = Priority(name=priority)
+            session.add(new_priority)
+            session.commit()
+            self.load_priorities()
