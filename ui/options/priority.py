aec615f2a828f43c9b087b8b43c322729099899a
Adam Dąbrowski <adam@codepy.pl>
2024-04-25 22:43:17 +0200

    dodano okno obslugi priorytatow

---
+++ ui/options/priority.py
@@ -0,0 +1,24 @@
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QListWidget, QPushButton, QInputDialog, QLineEdit
+
+from db import session, Priority
+
+
+class PriorityManagerApp(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Zarządzanie Priorytetami")
+        self.setGeometry(100, 100, 300, 200)
+
+        layout = QVBoxLayout()
+
+        self.priority_list = QListWidget()
+        layout.addWidget(self.priority_list)
+
+        self.load_priorities()
+        self.setLayout(layout)
+    def load_priorities(self):
+        self.priority_list.clear()
+        priorities = session.query(Priority).all()
+        for priority in priorities:
+            self.priority_list.addItem(str(priority.name))
